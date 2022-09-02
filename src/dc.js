39be871bd36ff6f61e293cbd2cf5e5e5ccecc5cb
melektron <developer@elektron.work>
2022-09-02 23:02:35 +0000

    restructured discord code

--- src/dc.js
+++ src/dc.js
@@ -2,6 +2,45 @@
 code for connecting to discord
 */
 
-export const sendMessage = (text) => {
-    console.log("New message: ", text);
+
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { token, botid } = require("./token.json");
+
+
+let client
+let channel
+
+
+const run = () => {
+    // Create a new client instance
+    client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages
+        ]
+    });
+
+    // When the client is ready, run this code (only once)
+    client.once('ready', () => {
+        console.log('Ready!');
+
+        channel = client.channels.cache.get("1015394508554371103");
+        channel.send("Hello, World!");
+    });
+
+    client.on("messageCreate", (message) => {
+        if (message.author.id === botid) return;
+
+        console.log(message);
+    });
+
+    // Login to Discord with your client's token
+    client.login(token);
 }
+
+const sendMessage = (text) => {
+    channel.send(text);
+}
+
+exports.run = run;
+exports.sendMessage = sendMessage;
