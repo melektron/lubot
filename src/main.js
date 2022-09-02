db082c38c73af541dd62803ce65a1300c9011e64
melektron <developer@elektron.work>
2022-09-02 21:45:23 +0000

    added discord login code

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
+/*
+ELEKTRON © 2022
+Written by melektron, DarkMetalMouse
+www.elektron.work
+02.09.22, 23:23
 
-console.log("hello World")
+*/
+
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require("./token.json");
+
+console.log(token);
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
